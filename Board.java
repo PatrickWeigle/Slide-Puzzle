a47d27f65c84a638635b7db5fc0dadc3d1556a76
pedrohenriquesanches <pedrohenrique_pi@hotmail.com>
2015-09-09 22:36:58 -0300

    Embaralhando cubos

--- Board.java
+++ Board.java
@@ -1,4 +1,4 @@
-  import java.awt.*;
+import java.awt.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
@@ -20,8 +20,21 @@ public class Board extends JPanel implements ActionListener {
     private int velocidade = 100;    
     private boolean isPlaying = false;    
     private String frase;
+    private Mesa posicoes = new Mesa();
     private Image mesa = new ImageIcon(this.getClass().getResource("images/table.jpg")).getImage();
-    
+    private Image num1 = new ImageIcon(this.getClass().getResource("images/1.png")).getImage();
+    private Image num2 = new ImageIcon(this.getClass().getResource("images/2.png")).getImage();
+    private Image num3 = new ImageIcon(this.getClass().getResource("images/3.png")).getImage();
+    private Image num4 = new ImageIcon(this.getClass().getResource("images/4.png")).getImage();
+    private Image num5 = new ImageIcon(this.getClass().getResource("images/5.png")).getImage();
+    private Image num6 = new ImageIcon(this.getClass().getResource("images/6.png")).getImage();
+    private Image num7 = new ImageIcon(this.getClass().getResource("images/7.png")).getImage();
+    private Image num8 = new ImageIcon(this.getClass().getResource("images/8.png")).getImage();
+
+    private Image[] nums = {num1,num2,null,num3,num4,num5,num6,num7,num8};
+
+    //private Image num9 = new ImageIcon(this.getClass().getResource("images/9.png")).getImage();
+
     public Board() {
 
         addKeyListener(new TAdapter());
@@ -32,12 +45,24 @@ public class Board extends JPanel implements ActionListener {
         frase = "Pressione ENTER para iniciar o jogo! ";
         timer = new Timer(velocidade, this);
         timer.start();
+        posiçoes();
     }
-    
+
     public void iniciar(){
-      
-    }     
-    
+
+    }
+
+    public void posiçoes() { 
+        Random random = new Random();
+        for (int i=0; i < (nums.length - 1); i++) { 
+            int j = random.nextInt(nums.length);
+            Image temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        } 
+        // Collections.sort(buttons);
+    }
+
     public boolean isEmpty(){
         if(inicio == null){
             return true;
@@ -50,40 +75,44 @@ public class Board extends JPanel implements ActionListener {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;    
         g2d.drawImage(mesa,-70,0,this);
+        for(int i=0;i<nums.length;i++){
+            g2d.drawImage(nums[i],posicoes.xX(i),posicoes.yY(i),this);
+        }
+
         Toolkit.getDefaultToolkit().sync();
         g.dispose();
-        
-    }    
-    
+
+    }  
+
     public void actionPerformed(ActionEvent e) {     
         repaint(); 
     }    
-    
+
     private class TAdapter extends KeyAdapter {
 
         public void keyPressed(KeyEvent e) {
-            
+
             // Obtém o código da tecla
             int key =  e.getKeyCode();
             //keyAnt = key;
             switch (key){
                 case KeyEvent.VK_ENTER:                    
-                    break;
-                    
+                break;
+
                 case KeyEvent.VK_LEFT:                  
-                    break;
-                    
+                break;
+
                 case KeyEvent.VK_RIGHT:                    
-                    break;
-                    
+                break;
+
                 case KeyEvent.VK_UP:                    
-                    break;
-                    
+                break;
+
                 case KeyEvent.VK_DOWN:                    
-                    break;
+                break;
             }           
-           
+
         }
     }
-    
+
 }
--- Mesa.java
+++ Mesa.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.ImageIcon;
 import java.util.Collections;
+import java.util.Random;
 
 /**
  * Write a description of class Snake here.
@@ -21,36 +22,60 @@ public class Mesa extends JPanel
     private int y;    
     private Image image;
     public static byte[] buttons = {1,2,3,4,5,6,7,8,9};
-    
+    private static final int[] X = {265,346,427,265,346,427,265,346,427};
+    private static final int[] Y = {150,150,150,231,231,231,312,312,312};
+    private byte[] pos = {0,1,2,3,4,5,6,7,8};
+
     public Mesa() {        
         x = 0;
         y = 0;
-       // Collections.sort(buttons);
+        Random random = new Random();
+        for (int i=0; i < (pos.length - 1); i++) { 
+            int j = random.nextInt(pos.length);
+            byte temp = pos[i];
+            pos[i] = pos[j];
+            pos[j] = temp;
+        } 
+        // Collections.sort(buttons);
     }
     
+    public int getPos(byte i){
+        return pos[i];
+    }
     
-    
+    public int getTamanho(){
+        return pos.length;
+    }
+
+    public int xX(int i){
+        return X[i];
+    }
+
+    public int yY(int i){
+        return Y[i];
+    }
+
     public void setIncrementX(int incrementX){
         this.incrementX = incrementX;
     }
-    
+
     public void setIncrementY(int incrementY){
         this.incrementY = incrementY;
     }
-   
+
     public int getIncrementX(){
         return this.incrementX;
     }
-    
+
     public int getIncrementY(){
         return this.incrementY;
     }
-    
+
     public void move() {          
         x += incrementX;
         y += incrementY;        
     }
-    
+
     public void setX(int x) {
         this.x = x;
     }
@@ -58,7 +83,7 @@ public class Mesa extends JPanel
     public void setY(int y) {
         this.y = y;
     } 
-    
+
     public int getX() {
         return x;
     }
@@ -66,14 +91,10 @@ public class Mesa extends JPanel
     public int getY() {
         return y;
     }    
-    
 
     public Image getImage() {
-        
         return image;
-        
     }   
-    
     public Image getImageFloor(){
         ImageIcon ii = new ImageIcon(this.getClass().getResource("images/floor.jpg"));
         return ii.getImage();        
